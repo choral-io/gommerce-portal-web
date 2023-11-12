c56beaa8dfecc9bd09c806e50bf371f78c320910
Tiscs <tiscs@live.com>
2023-11-12 12:03:33 +0800

    Update dependencies

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,15 @@
 import type { MetaFunction } from "@remix-run/node";
 
-export const meta: MetaFunction = () => [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-];
+export const meta: MetaFunction = () => {
+    return [
+        { title: "New Remix App" },
+        { name: "description", content: "Welcome to Remix!" },
+    ];
+};
 
 export default function Index() {
     return (
-        <>
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
             <h1>Welcome to Remix</h1>
             <ul>
                 <li>
@@ -34,6 +36,6 @@ export default function Index() {
                     </a>
                 </li>
             </ul>
-        </>
+        </div>
     );
 }
