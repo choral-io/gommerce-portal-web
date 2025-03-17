6ab23c008af67b0947c50a34d59faa9807365131
Tiscs <tiscs@live.com>
2025-03-17 20:00:59 +0800

    Upgrade to tailwindcss v4

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -17,6 +18,7 @@ export default defineConfig({
                 v3_routeConfig: true,
             },
         }),
+        tailwindcss(),
         tsconfigPaths(),
     ],
     resolve: {
